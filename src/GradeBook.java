1f8eeef7d2e009df8755f395249a735ad95944be
Kacper Sokol <80675@student.pb.edu.pl>
2022-11-29 09:25:47 +0100

    dodałem klasy

---
+++ src/GradeBook.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public class GradeBook {
+
+    ArrayList<Subject> subjects = new ArrayList<Subject>();
+}
---
+++ src/Subject.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public class Subject {
+    String name;
+    ArrayList<Integer> grades = new ArrayList<Integer>();
+}
